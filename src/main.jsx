f75404e95138787d502903f3ce3424a4bcddbd21
thomasgdrive20-prog <thomasgdrive.20@gmail.com>
2026-05-11 18:25:23 -0300

    mais

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// A linha abaixo foi comentada para ignorar o erro do arquivo sumido
+// import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
