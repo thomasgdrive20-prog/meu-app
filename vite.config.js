b75a7b367e6702b30d357d62a3ad76eed012b238
thomasgdrive20-prog <thomasgdrive.20@gmail.com>
2026-05-11 18:03:26 -0300

    update noww

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    // ESTA LINHA É A QUE RESOLVE A TELA PRETA
+    // Resolve o erro de carregamento do PDF [cite: 287]
     include: ['pdfjs-dist'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/pdfjs-dist/],
-    },
   },
 })
